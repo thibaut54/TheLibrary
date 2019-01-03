9c09d4eb689806405ccf5239ae0c9b6097480dd6
JMilton <john_milton100@hotmail.com>
2019-01-03 01:37:17 +0100

    feat<webservice>: fix webservice: it works (code needs to be cleaned)!

--- webservice/src/main/java/org/thibaut/thelibrary/webservice/webservice/SearchBookWebServiceImpl.java
+++ webservice/src/main/java/org/thibaut/thelibrary/webservice/webservice/SearchBookWebServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.thibaut.thelibrary.webservice.configuration.WebApplicationContextLocator;
 
 import javax.annotation.PostConstruct;
 import javax.jws.WebMethod;
@@ -31,19 +32,19 @@ public class SearchBookWebServiceImpl extends AbstractWebService implements Sear
 		return getServiceFactory().getBookService().getBookTitle( id );
 	}
 
-//	public SearchBookWebServiceImpl() {
-//		AutowiredAnnotationBeanPostProcessor bpp = new AutowiredAnnotationBeanPostProcessor();
-//		WebApplicationContext currentContext = WebApplicationContextLocator.getCurrentWebApplicationContext();
-//		bpp.setBeanFactory(currentContext.getAutowireCapableBeanFactory());
-//		bpp.processInjection(this);
-//	}
-//
-//	// alternative constructor to facilitate unit testing.
-//	protected SearchBookWebServiceImpl( ApplicationContext context) {
-//		AutowiredAnnotationBeanPostProcessor bpp = new AutowiredAnnotationBeanPostProcessor();
-//		bpp.setBeanFactory(new DefaultListableBeanFactory(context));
-//		bpp.processInjection(this);
-//	}
+	public SearchBookWebServiceImpl() {
+		AutowiredAnnotationBeanPostProcessor bpp = new AutowiredAnnotationBeanPostProcessor();
+		WebApplicationContext currentContext = WebApplicationContextLocator.getCurrentWebApplicationContext();
+		bpp.setBeanFactory(currentContext.getAutowireCapableBeanFactory());
+		bpp.processInjection(this);
+	}
+
+	// alternative constructor to facilitate unit testing.
+	protected SearchBookWebServiceImpl( ApplicationContext context) {
+		AutowiredAnnotationBeanPostProcessor bpp = new AutowiredAnnotationBeanPostProcessor();
+		bpp.setBeanFactory(new DefaultListableBeanFactory(context));
+		bpp.processInjection(this);
+	}
 
 }
 
